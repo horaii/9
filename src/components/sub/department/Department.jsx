be4b4297de38d29e2c7fc1c34fa0dcab3f03a9ef
horaii <gxhzxtxad@gmail.com>
2023-10-11 14:50:44 +0900

    7

--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -9,10 +9,12 @@ export default function Department() {
 
 	useEffect(() => {
 		fetch(`${path}/DB/department.json`)
-			.then((data) => data.json())
-			.then((json) => {
-				setDepartment(json.members);
-			});
+		.then((data) => data.json()) //fetch문에 대한 응답 성공시
+		.catch((err) => console.log(err)) //fetch문에 대한 응답 실패시
+		.then((json) => {
+			setDepartment(json.members); //json데이터 변환에 대한 응답 성공시
+		})
+		.catch((err) => console.log(err)); //json데이터 변환에 대한 응답 실패시
 	}, []);
 
 	return (
@@ -66,9 +68,41 @@ export default function Department() {
 	
 	4.fetch문을 useEffect안쪽에서 호출하는 이유
 	
-	- 가상돔 생성은 리액트 기반의 스크립트가 처리해주시면 외부 데이터를 가져오는 것은 web API (브라우저)가 처리하기 때문에 
+	- 가상돔 생성은 리액트 기반의 스크립트가 처리해주지만 외부 데이터를 가져오는 것은 web API (브라우저)가 처리하기 때문에 
 	
 	- 컴포넌트가 실제 브라우저상에 마운트가되고 브라우저가 작업 준비가 되야지만 fetch를 실행할 수 있기 때문에 
 	
 	- useEffect컴포넌트가 마운트 되야지만 CSR방식으로 외부 데이터를 가져올 수 있음.
+
+	컴포넌트 작성 순서
+	import로 외부 모듈, 컴포넌트 불러오기
+	export default funciton 컴포넌트이름(){
+		필요시 hook 호출 (hook안에서 hook호출 불가, 핸들러안쪽에서 호출불가)
+		필요시 핸들러함수 정의
+		useEffect(()=>{
+			핸들러함수 호출 (fetch, 이벤트연결)
+		})
+		return JSX
+	}
+
+	fetch: ES6에서 기본문법으로 포함된 동기적으로 외부 데이터를 가져오는 내장함수
+	fetch는 promise반환
+	promise가 반환되야지 .then구문 호출가능
+	.then구문을 호출해야지만 동기적으로 다음코드 실행가능
+	JSON (Javascript Object Notation) 자바스크립트 객체 표현식
+
+	promise : 데이터의 상태값을 추적할 수 있는 객체
+	promise의 3가지 상태
+	pending: 요청을 보내고 응답을 받기까지의 대기상태
+	fulfilled: pending이 끝나고 요청에 대한 응답을 성공적으로 받은 상태
+	rejected: pending이 끝나고 요청에 대한 응답을 받긴 하지만 에러를 반환받은 상태를 rejected
+
+
+	- 자바스크립트의 객체를 문자열형태로 관리하는 데이터 형식
+	- 문자형식으로 되어있는 JSON는 다시 객체형식으로 변환 (parsing)
+
+	promise .then 구문을 구조적으로 짜는 방법 (aync amait)
+	async amait 사용조건
+	-promise 반환함수를 wrapping처리
+	-wrap
 */
